32b6ac1121ff53d100c79aeaa188300b12fd652c
Orandifelix <125347734+Orandifelix@users.noreply.github.com>
2023-12-04 15:11:22 +0300

    config google auth

--- App/Pages/Login.js
+++ App/Pages/Login.js
@@ -1,9 +1,23 @@
-import { View, Text, Image, StyleSheet } from "react-native";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import Color from "../Shared/Color";
 import { AntDesign } from "@expo/vector-icons";
+import * as webBrowser  from 'expo-web-browser'
+import * as Google from 'expo-auth-session/providers/google'
 
 const Login = () => {
+
+  webBrowser.maybeCompleteAuthSession();
+const [request, response, promptAsync] = Google.useAuthRequest({
+  androidClientId:
+    "572768065371-752a5caejk2o5gg62c6h9n30c80c9vr3.apps.googleusercontent.com",
+  expoClientId:
+    "572768065371-su6dcilfj684fvt7k2m3gjgqts2nndsp.apps.googleusercontent.com",
+  redirectUri: "https://auth.expo.io/orandifelix/codebox",
+});
+
+
+
   return (
     <View style={styles.container}>
       <Image source={require("./../Assets/Images/login.png")} />
@@ -12,10 +26,15 @@ const Login = () => {
         <Text style={{ textAlign: "center", marginTop: 80, fontSize: 25 }}>
           Login/Signup
         </Text>
-        <View style={styles.button}>
-          <AntDesign name="google" size={24} color="white"  style={{marginRight:10}}/>
-          <Text style={{color:Color.white}}>Sign in With Google</Text>
-        </View>
+        <TouchableOpacity style={styles.button} onPress={()=>promptAsync()}>
+          <AntDesign
+            name="google"
+            size={24}
+            color="white"
+            style={{ marginRight: 10 }}
+          />
+          <Text style={{ color: Color.white }}>Sign in With Google</Text>
+        </TouchableOpacity>
       </View>
     </View>
   );
